8736f4cdf2e884e1bee5c5bd023a4864f33c7d0c
toneloke <david.guerrero87@live.com>
2017-05-31 09:19:49 -0400

    added omni auth gem

--- Module-2/flowers/app/controllers/sessions_controller.rb
+++ Module-2/flowers/app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
      flash.now.alert = "invalid login credentials"
      redirect_to "/"
     end
- end
+end
 
   def destroy
      session[:user_id] = nil
