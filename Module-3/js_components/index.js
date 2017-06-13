48c7b8938435e93835664dc107812ed13edc5e3b
toneloke <david.guerrero87@live.com>
2017-06-13 10:32:17 -0400

    added web comp

--- Module-3/js_components/index.js
+++ Module-3/js_components/index.js
@@ -55,7 +55,6 @@ window.onload = function () {
       switch (clickedEl.className) {
         case 'glyphicon glyphicon-remove':
           this.todoList.removeTodo(clickedEl.parentNode.childNodes[2].innerText)
-          clickedEl.parentNode.classList.add('zoomOutDown')
           clickedEl.parentNode.parentNode.removeChild(clickedEl.parentNode)
           break
         case 'glyphicon glyphicon-star':
@@ -80,55 +79,3 @@ window.onload = function () {
 
   tonysTodoList.render()
 }
-
-class AppDrawer extends HTMLElement {
-
-  // A getter/setter for an open property.
-  get open () {
-    return this.hasAttribute('open')
-  }
-
-  set open (val) {
-    // Reflect the value of the open property as an HTML attribute.
-    if (val) {
-      this.setAttribute('open', '')
-    } else {
-      this.removeAttribute('open')
-    }
-    this.toggleDrawer()
-  }
-
-  // A getter/setter for a disabled property.
-  get disabled () {
-    return this.hasAttribute('disabled')
-  }
-
-  set disabled (val) {
-    // Reflect the value of the disabled property as an HTML attribute.
-    if (val) {
-      this.setAttribute('disabled', '')
-    } else {
-      this.removeAttribute('disabled')
-    }
-  }
-
-  // Can define constructor arguments if you wish.
-  constructor () {
-    // If you define a ctor, always call super() first!
-    // This is specific to CE and required by the spec.
-    super()
-
-    // Setup a click listener on <app-drawer> itself.
-    this.addEventListener('click', e => {
-      // Don't toggle the drawer if it's disabled.
-      if (this.disabled) {
-        return
-      }
-      this.toggleDrawer()
-    })
-  }
-
-  toggleDrawer () {}
-}
-
-customElements.define('app-drawer', AppDrawer)
